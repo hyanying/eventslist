b042f136dcc36b5ed658e317a6118034e327dea1
hyanying <yan.and.ying@gmail.com>
2016-08-23 18:40:50 +0100

    added countdown timer

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,46 @@
 module EventsHelper
+
+	def timeLeft(eventDate,eventTime)
+		
+		currentDate = Date.today
+		currentTime = Time.now
+		eventDateTime = eventDate + eventTime.seconds_since_midnight.seconds
+		#eventDateTime = eventDateTime.change(:offset => "+0800")
+
+		timeLeft = eventDateTime - Time.now 
+		result = ""
+
+		if timeLeft < 0
+			return "EXPIRED"	
+		
+		else 
+					
+			daysLeft = (eventDate - currentDate - 1).to_i 
+
+			if daysLeft == 1
+				result += daysLeft.to_s + " Day "
+
+			elsif daysLeft > 1
+				result += daysLeft.to_s + " Days "
+			end
+			
+			hoursLeft = (((eventDateTime - Time.now) /3600).round) - (daysLeft*24)
+
+			if hoursLeft == 1
+				result += hoursLeft.to_s + " Hour "
+
+			elsif hoursLeft > 1
+				result += hoursLeft.to_s + " Hours "
+			end
+
+		
+			
+		end			
+
+		result += "Left"
+
+		return result;
+
+	end
+
 end
